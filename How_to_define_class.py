199fb396173a65705afe7d8389d96db2367f4d4e
vdixitav <vishwasraodixita@gmail.com>
2024-03-10 22:01:58 +0530

    Class

---
+++ How_to_define_class.py
@@ -0,0 +1,20 @@
+# We can define class by using class keyword
+
+
+#SYNTAX:
+
+# class className:
+ # """DocumentaionString"""
+
+#Documentaion String represent description of the class
+#Within the class doc. string is alsways optional
+# We can get doc string by using following 2 ways
+
+# 1)print(classname.__doc__)
+# 2)help(classname)
+
+class Student:
+    print("dixita")
+    """This class is required all documents"""
+    print("Student abc")
+#help(Student)    
