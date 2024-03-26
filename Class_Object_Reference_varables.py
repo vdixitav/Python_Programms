1a0f19294248793923b16ea9ed1f05defb75a214
vdixitav <vishwasraodixita@gmail.com>
2024-03-26 15:14:54 +0530

    daily practice

--- Class_Object_Reference_varables.py
+++ Class_Object_Reference_varables.py
@@ -1,20 +1,34 @@
 
 class Student:
-    def __init__(self) -> None:
-        self.name="Python"
-        self.rollno=101
-        self.marks=80
+    def __init__(self,x,y,z) -> None:
+        self.name='python'
+        self.rollno=000
+        self.marks=x
 
     def talk(self):
         print("Hello there,My name is",self.name)
         print("My roll no",self.rollno)
         print("My marks",self.marks)
 
-s=Student()
+s1=Student("",101,80)
 
-print(s.marks)
-print(s.name)
-print(s.rollno)
+print(s1.name,s1.rollno,s1.marks)
 
-s.talk()
+
+
+
+
+# print("S1:",s1.name)
+# s2=Student()
+# print("S2",s2.name)
+
+# print('Adress of s1',id(s1))
+# print("Adress of s2",id(s2))
+
+
+# print(s.marks)
+# print(s.name)
+# print(s.rollno)
+
+# s.talk()
 
