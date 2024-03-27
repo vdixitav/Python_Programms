3d36d8fcabe6b23f789dd4c81d4a06297a3a1caf
vdixitav <vishwasraodixita@gmail.com>
2024-03-27 09:25:35 +0530

    daily practice

---
+++ self.py
@@ -0,0 +1,29 @@
+# class Students :
+#     def __init__(self,name,rollno,marks):
+#          self.name=name
+#          self.rollno=rollno
+#          self.marks=marks
+
+#     def talk(self):
+#          print("hello i am :",self.name)
+#          print("my rollno is :",self.rollno)
+#          print("my marks are :", self.marks)     
+
+# s1=Students("Mani",45,90)
+# s2=Students("Bunny",60,20)
+
+# s1.talk()
+# s2.talk()
+
+
+
+class Test:
+    def __init__(self) -> None:
+        print("constructor")
+
+    # def m1(self,x):
+    #     print("method")
+
+t=Test(10)
+#t.m1(10)             
+       
