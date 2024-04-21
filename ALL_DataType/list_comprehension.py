735588fb2ae43ef4baf5cf370c13e7c76d02e5db
vdixitav <vishwasraodixita@gmail.com>
2024-04-21 14:28:41 +0530

    daily practice

---
+++ ALL_DataType/list_comprehension.py
@@ -0,0 +1,28 @@
+# mystr='WELCOME'
+# mystr1=[ i for i in mystr] # iterating through string using list comprehension
+# print(mystr1)
+
+# display all even number between 40
+
+
+
+# mystr2=[ i for i in range(20) if i %2==0]
+# print(mystr2)
+
+# multiple whole no by 10
+
+lst1=[2,3,4,5,6,7,8]
+lst1=[i*10 for i in lst1]
+print(lst1)
+
+#extract numbers from string
+mystr='one 1 two 2 three 3 four 4 five 5 six 6'
+numbers=[i for i in mystr if i.isdigit()]
+print(numbers)
+
+
+
+
+
+
+ 
