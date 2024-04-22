07ba9cb519e2a300358d3cd445bedf95fcbc7676
vdixitav <vishwasraodixita@gmail.com>
2024-04-22 17:26:28 +0530

    daily practice

--- ALL_DataType/Tuples.py
+++ ALL_DataType/Tuples.py
@@ -1,14 +1,25 @@
 # Tuple creation
 
-tup1=()
+# tup1=()
 
-tup2=(10,20,30)
+# tup2=(10,20,30)
 
-tup3=(10.7,6,4,3,9.3)
-tup4=('one','two','three')
+# tup3=(10.7,6,4,3,9.3)
+# tup4=('one','two','three')
  
-tup5=('Asif',1,[89,7,3,3])
+# tup5=('Asif',1,[89,7,3,3])
 
-a=len(tup5)
+# a=len(tup5)
+# print(a)
+
+# tuple index
+
+# tup2=(10,20,30)
+# a=tup2[2]
+# print(a)
+ 
+# Tuple slicing
+
+mytuple=('one','two','three','four','five','six','seven','eight')
+a=mytuple[0:3]
 print(a)
-
