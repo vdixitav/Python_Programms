1ce08d4cc629f789d46693383cc3225a892eaca1
vdixitav <vishwasraodixita@gmail.com>
2024-04-24 23:03:49 +0530

    daily practice

---
+++ container.py
@@ -0,0 +1,18 @@
+# list=['me','you','we','are']
+# # a='me' in list
+# # print(a)
+
+# list_iter=iter(list)
+# print(next(list_iter))
+# print(next(list_iter))
+# print(next(list_iter))
+# print(next(list_iter))
+# print(next(list_iter))
+
+# list_iter=iter(list)
+# for i in list_iter:
+    # print(i)
+tuple=('me','you','we','are')
+for i in tuple:
+    print(i)
+
