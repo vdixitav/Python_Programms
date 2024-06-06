48bf74712539fe964ef0616585b6c81ced675741
vdixitav <vishwasraodixita@gmail.com>
2024-06-06 21:58:57 +0530

    daily practice

--- Python_daily/new start doc.py
+++ Python_daily/new start doc.py
@@ -17,3 +17,35 @@
 
 # string
 
+# a='hello team'
+# print(a[1])
+
+#looping hrought string
+# for x in 'banana':
+#     print(x)
+
+#len()
+# a='hello team'
+# print(len(a))
+
+
+# a='hello team'
+# print("free"in a)
+
+# a='hello team'
+# if 'free' in a:
+#     print('yes m pagal')
+
+# else:
+#     print("me")    
+
+
+# txt = "The best things in life are free!"
+# # print("yes" in txt)
+
+# if 'yes'not in txt:
+#     print("me")
+# else:
+#     print("y")    
+
+
