36f5b0cf988f2f4c3d1f632c2ff0170cb0fe7cdb
vdixitav <vishwasraodixita@gmail.com>
2024-06-06 21:37:21 +0530

    daily practice

---
+++ Python_daily/new start doc.py
@@ -0,0 +1,19 @@
+# always give a float value and // will give int
+# to calculate remainder use %
+
+# a=17/3
+# print(a)
+
+# b=17//10
+# print(b)
+
+# c=17%3
+# print(c)
+
+# use ** operator to find power
+
+# a=5**2
+# print(a)
+
+# string
+
